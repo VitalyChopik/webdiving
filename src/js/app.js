89a63ac9e73c9b913a4a8698fc1e961c51fe561a
VitalyChopik <leo.space97@gmail.com>
2023-04-19 13:35:09 +0300

    feat: сделано несколько секций и добавены новые файлы

--- src/js/app.js
+++ src/js/app.js
@@ -15,9 +15,7 @@ import {
   menuInit,
 } from './modules'
 
-import animateProgressBar from './helpers/animateProgressBar'
 
-animateProgressBar();
 /* Раскомментировать для использования */
 // import { MousePRLX } from './libs/parallaxMouse'
 
@@ -42,7 +40,7 @@ isWebp()
 // addLoadedClass();
 /* Модуль для работы с меню (Бургер) */
 /* Раскомментировать для использования */
-// menuInit()
+menuInit()
 
 /* Библиотека для анимаций ===============================================================================
  *  документация: https://michalsnik.github.io/aos
@@ -69,3 +67,116 @@ isWebp()
 /* Раскомментировать для использования */
 // togglePopupWindows()
 // =======================================================================================================
+
+
+function animateValue(id, start, end, duration) {
+  if (start === end) return;
+  const range = end - start;
+  let current = start;
+  const increment = end > start ? 1 : -1;
+  const stepTime = Math.abs(Math.floor(duration / range));
+  const obj = document.getElementById(id);
+  const timer = setInterval(function () {
+    current += increment;
+    if (!obj) {
+      clearInterval(timer);
+      return;
+    } else {
+      obj.innerHTML = current;
+    }
+    if (current === end) {
+      clearInterval(timer);
+    }
+  }, stepTime);
+}
+
+const forward = document.getElementById('forward')
+
+// функция определяет нахождение элемента в области видимости
+// если элемент видно - возвращает true
+// если элемент невидно - возвращает false
+function isOnVisibleSpace(element) {
+  if (element) {
+    let bodyHeight = window.innerHeight;
+    let elemRect = element.getBoundingClientRect();
+    let offset = elemRect.top;// - bodyRect.top;
+    if (offset < 0) return false;
+    if (offset > bodyHeight) return false;
+    return true;
+  }
+}
+
+// глобальный объект с элементами, для которых отслеживаем их положение в зоне видимости
+let listenedElements = [];
+// обработчик события прокрутки экрана. Проверяет все элементы добавленные в listenedElements
+// на предмет попадания(выпадения) в зону видимости
+window.onscroll = function () {
+  listenedElements.forEach(item => {
+    if (!item.el) return;
+    // проверяем находится ли элемент в зоне видимости
+    let result = isOnVisibleSpace(item.el);
+
+    // если элемент находился в зоне видимости и вышел из нее
+    // вызываем обработчик выпадения из зоны видимости
+    if (item.el.isOnVisibleSpace && !result) {
+      return;
+    }
+    // если элемент находился вне зоны видимости и вошел в нее
+    // вызываем обработчик попадания в зону видимости
+    if (!item.el.isOnVisibleSpace && result) {
+      item.el.isOnVisibleSpace = true;
+      item.inVisibleSpace(item.el);
+      return;
+    }
+  });
+}
+
+
+function onVisibleSpaceListener(elementId, cbIn, cbOut) {
+  // получаем ссылку на объект элемента
+  let el = document.getElementById(elementId);
+  // добавляем элемент и обработчики событий
+  // в массив отслеживаемых элементов
+  listenedElements.push({
+    el: el,
+    inVisibleSpace: cbIn,
+    outVisibleSpace: cbOut
+  });
+}
+
+onVisibleSpaceListener("value1",
+  el => {
+    // функция вызываемая при попадании элемента в зону видимости
+    // тут вставляем код запуска анимации
+    forward.beginElement()
+    animateValue("value1", 1, 250, 2000)
+    animateValue("value2", 1, 20, 2000)
+    animateValue("value3", 1, 14, 2000)
+    animateValue("value4", 1, 8, 2000)
+  });
+
+
+const tabsItems = document.querySelectorAll('.about__tabs-item'),
+  tabsContent = document.querySelectorAll('.about__box');
+tabsItems[0].classList.add('active');
+tabsContent[0].classList.add('active');
+
+// Добавляем обработчик события "click" для каждого элемента в tabsItems
+tabsItems.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    // Добавляем класс "active" к элементу в tabsContent с тем же индексом
+    tabsContent.forEach((content, contentIndex) => {
+      if (contentIndex === index) {
+        content.classList.add('active');
+      } else {
+        content.classList.remove('active');
+      }
+    });
+
+    // Добавляем класс "active" к нажатому элементу в tabsItems
+    tabsItems.forEach((tab) => {
+      tab.classList.remove('active');
+    });
+    item.classList.add('active');
+  });
+});
